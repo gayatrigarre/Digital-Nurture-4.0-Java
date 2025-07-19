a59308613f308bf8c67f91b5df59e20a47ae6ac7
gayatrigarre <2200100025iot@gmail.com>
2025-07-19 21:21:24 +0530

    Add files via upload

---
+++ week-5_Microservices/loan1/loan1/src/test/java/com/cognizant/loan1/Loan1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.loan1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Loan1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
